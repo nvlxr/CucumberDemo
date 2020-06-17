aa22cae704d44ec1ddbbb41e62010501bfc9e616
nvlxr <thang.phan@live.com>
2020-06-17 17:53:27 +0700

    Initial commit

---
+++ src/test/java/stepdefs/DemoDefinitions.java
@@ -0,0 +1,13 @@
+package stepdefs;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.PendingException;
+public class DemoDefinitions {
+    @Given("^I have a configured Cucumber-JVM project$")
+    public void i_have_a_configured_cucumber_jvm_project() throws Exception {throw new PendingException();}
+    @When("^I run it within my IDE$")
+    public void i_run_it_within_my_ide() throws Exception{throw new PendingException();}
+    @Then("^I will be able to run connected step definitions$")
+    public void i_will_be_able_to_run_connected_step_definitions() throws Exception{throw new PendingException();}
+}
